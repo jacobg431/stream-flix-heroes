4870506663a339ed5caffc713f0a8c8f6131436c
Jacob Gomez Hansen <jacobg431@gmail.com>
2026-01-12 14:50:13 +0100

    Added source code to main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,44 @@
-#include <string>
 #include <iostream>
+#include <libpq-fe.h>
+
 
 int main()
 {
-    std::cout << "Program is running" << std::endl;
+const char* conninfo = "host=localhost port=5432 dbname=cpp_demo user=postgres password=YOUR_PASSWORD";
+
+
+PGconn* conn = PQconnectdb(conninfo);
+
+
+if (PQstatus(conn) != CONNECTION_OK)
+{
+std::cerr << "Connection failed: " << PQerrorMessage(conn);
+PQfinish(conn);
+return 1;
+}
+
+
+PGresult* res = PQexec(conn, "SELECT id, name FROM users");
+
+
+if (PQresultStatus(res) != PGRES_TUPLES_OK)
+{
+std::cerr << "Query failed: " << PQerrorMessage(conn);
+PQclear(res);
+PQfinish(conn);
+return 1;
+}
+
+
+int rows = PQntuples(res);
+for (int i = 0; i < rows; ++i)
+{
+std::cout << "ID: " << PQgetvalue(res, i, 0)
+<< ", Name: " << PQgetvalue(res, i, 1) << '\n';
+}
+
+
+PQclear(res);
+PQfinish(conn);
+return 0;
 }
