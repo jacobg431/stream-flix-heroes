713ddc12987bc195d344ef5ca258e6e8982f0a75
Jacob Gomez Hansen <jacobg431@gmail.com>
2026-01-19 13:13:38 +0100

    CREATE: Added interface for user review repository

---
+++ src/Repositories/IUserReviewRepository.hpp
@@ -0,0 +1,18 @@
+#pragma once
+#include "../Models/UserReview.hpp"
+#include "../Models/User.hpp"
+#include <vector>
+#include <string>
+
+class IUserReviewRepository
+{
+public:
+    virtual ~IUserReviewRepository() = default;
+
+    virtual std::vector<UserReview> selectReviewsByUser(const User &user) const = 0;
+
+    virtual std::vector<UserReview> selectReviewsOfMovie(const User &user) const = 0;
+
+protected:
+    std::string queryWhereClause;
+};
